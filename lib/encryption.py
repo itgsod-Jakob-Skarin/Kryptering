05ca95e6e8ac63d93216e43c4179e1dc096ef979
itgsod-Jakob-Skarin <jakob.skarin@elev.it-gymnasiet.se>
2014-05-13 12:11:46 +0200

    encrypt
    Början för att encrypta vår cleartext.

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,20 +1,34 @@
 def encrypt(cleartext, offset):
-    letters1 = ['a', ' ', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
-    letters2 = ['o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    letters = letters1 + letters2
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     if len(cleartext) == 0:
         raise ValueError('can not encrypt empty string')
     if offset == 0:
         raise ValueError('offset must not be zero')
-    for letter in cleartext:
-        new_letter = letters.index(letter) + offset
+    output = ''
+    if alphabet.index(char) + offset > 25:
+        alphabet.index(char) + offset - 26
 
-    #new_cleartext = ''
-    for new_letter
-        add new_letter in new_cleartext
-     bokstav i x, + y for ny bokstav
-    return cleartext.upper()
+    for char in cleartext:
+        if char == ' ':
+            output += ' '
+        else:
+            new_pos = alphabet.index(char) + offset
+            output += alphabet[new_pos]
+    return cleartext.upper
+
+
+#If pos + offset > 25:
+#pos + offset - 26:
+
+    # for letter in cleartext:
+    #     new_letter = letters.index(letter) + offset
+    #
+    # #new_cleartext = ''
+    # for new_letter
+    #     add new_letter in new_cleartext
+    #  bokstav i x, + y for ny bokstav
+    # return cleartext.upper()
 
 
 print encrypt('the quick brown fox jumps over the lazy dog', 3)
-
