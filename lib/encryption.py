8101aa149a3bf2084b6e6d1fff15a8cfbe382332
itgsod-Jakob-Skarin <jakob.skarin@elev.it-gymnasiet.se>
2014-05-27 09:02:24 +0200

    Klar + Kommentarer
    Kommentarer a la Jakob

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,16 +1,17 @@
-def encrypt(cleartext, offset):
+def encrypt(cleartext, offset): # två argument, text och förskutning
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    if len(cleartext) == 0:
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] #veta vilken lista det förskuts i, eftersom den
+    # inte själv kan alfabetet
+    if len(cleartext) == 0: # om text in är tom så ska/kan den inte ändra något
         raise ValueError('can not encrypt empty string')
-    if offset == 0:
+    if offset == 0: # om förskutningen är 0 händer inget, error måste inte ges egentligen.
         raise ValueError('offset must not be zero')
-    output = ''
+    output = '' # gör en output redo
     for char in cleartext:
         char = char.lower()
         if char == ' ':
-            output += ' '
-        elif alphabet.index(char) + offset > 25:
+            output += ' ' #gör så att space fortsätter vara space, annars behöver man ha med mellanrum i alfabetet
+        elif alphabet.index(char) + offset > 25: # det här hindrar det från att bugga när den går mot slutet av listan
             new_pos =+ alphabet.index(char) + offset - 26
             output += alphabet[new_pos]
         else:
